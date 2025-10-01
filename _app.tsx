aab573779f0b19c084905a1a45ee5ffacc6ca685
nolaniu <niujiale0314@163.com>
2025-10-02 01:23:40 +0800

    delete i18n

--- _app.tsx
+++ _app.tsx
@@ -1,8 +1,8 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
-import { appWithTranslation } from 'next-i18next';
+// import { appWithTranslation } from 'next-i18next';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-export default appWithTranslation(MyApp);
+// export default appWithTranslation(MyApp);

240eeeb112eb25945e58b5b9f9c2ce6217d9910b
nolaniu <niujiale0314@163.com>
2025-10-02 01:39:00 +0800

    de i18n

--- next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-// next-i18next.config.js
-module.exports = {
-  i18n: {
-    locales: ['zh', 'en'],
-    defaultLocale: 'zh',
-    localeDetection: true,
-  },
-  reloadOnPrerender: process.env.NODE_ENV === 'development',
-};

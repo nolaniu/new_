90a25c67d4276d07081331fdecc1ed5c03bc1bb1
nolaniu <niujiale0314@163.com>
2025-10-02 10:25:52 +0800

    link GA4

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,20 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9562574307655830"
           crossOrigin="anonymous"
         ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TLYSVDL4KW"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TLYSVDL4KW');
+            `,
+          }}
+        />
       </Head>
       <body className="bg-gradient-to-br from-slate-50 via-white to-slate-100 text-slate-900">
         <Main />
